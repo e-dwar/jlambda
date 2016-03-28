3136a619832ec9c4544fad4bb22f4d00de46c1cb
e-dwar <mailfork-github2@yahoo.com>
2016-03-28 19:00:04 +0200

    Readme

--- minit.js
+++ minit.js
@@ -1,11 +1,14 @@
 (function () {
     var COLOR = "#1FB61A";
     var input = document.getElementsByTagName("pre")[0].lastChild;
-    var url = "http://heterotopos.free.fr/index.php?post/2010/05/24/Le-lambda-calcul-pour-les-nuls";
+    var url1 = "http://heterotopos.free.fr/index.php?post/2010/05/24/Le-lambda-calcul-pour-les-nuls";
+    var url2 = "https://raw.githubusercontent.com/e-dwar/jlambda/master/tab3-3e92f.jpg";
     mini.info = info;
     mini.info("" 
-    + "<a href=\"" + url + "\" style=\"color:" + COLOR + "\">"
+    + "<a href=\"" + url1 + "\" style=\"color:" + COLOR + "\">"
     + "Le lambda calcul pour les nuls!"
+    + "</a> <a href=\"" + url2 + "\" style=\"color:" + COLOR + "\">"
+    + "Help!"
     + "</a>"
     );
     function info (msg) {
